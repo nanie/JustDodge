a638564f8dcb4e0e70cd21969e0fc57cca5ce398
Camilla "Nanie" Camatta <camillacamatta@gmail.com>
2019-11-25 15:25:05 -0300

    Player controllers
     - Players controller
     - Camera Smooth follow
     - Customization screen

---
+++ Assets/_MyApp/Scripts/CameraSmoothFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSmoothFollow : MonoBehaviour
+{
+    //Player transform
+    private Transform player;
+    //Camera velocity
+    private Vector3 velocity = Vector3.zero;
+
+    [Tooltip("How smooth is the movement of camera")]
+    public float smoothTime = 0.3F;
+
+    void Start()
+    {
+        //Find player gameobject by tag
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    void FixedUpdate()
+    {
+        //Set target position to player's position
+        Vector3 targetPosition = player.position;
+        //Keep the current Z position
+        targetPosition.z = transform.position.z;
+        //Follow player smoothly
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
---
+++ Assets/_MyApp/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    [Tooltip("Player movement speed")]
+    public float speed = 5f;
+
+    //Character Animator
+    private Animator animator;
+
+    //Character Rigidy Body
+    private Rigidbody2D rigidBody;
+
+    //Horizontal and vertical axis from player input
+    private Vector2 direction;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        //Find input axis
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+
+        //Animate character to match movement
+        animator.SetBool("walking", direction.sqrMagnitude > 0);
+
+        //Invert rect so character looks to right whn walking right
+        if (direction.x > 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else if (direction.x < 0)
+            transform.localScale = new Vector3(1, 1, 1);
+
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + direction * speed * Time.fixedDeltaTime);
+    }
+}
